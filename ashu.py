f8222f79a61f37acd3cd158b5ffa11f757e266ab
adityasingh9452 <shivam8299502857@gmail.com>
2025-05-17 20:31:55 +0530

    Update ashu.py

--- ashu.py
+++ ashu.py
@@ -105,3 +105,8 @@ def index():
 
 if __name__ == '__main__':
     app.run(debug=True)
+import os
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))  # Use PORT env var or default to 5000
+    app.run(host='0.0.0.0', port=port)
